680dc369058b0274dd7053e194cc9316539fd2fa
Bernard Maisel <ntjust26@gmail.com>
2018-09-22 19:56:33 +0900

    site redesign

--- ricemine.rb
+++ ricemine.rb
@@ -17,11 +17,11 @@ end
 
 helpers do
   def is_special_key?(key)
-    ["pic", "pic2", "pic3", "tier",  "stars", "type", "element"].include?(key.to_s)
+    ["pic", "pic2", "pic3", "tier", "stars", "type", "element"].include?(key.to_s)
   end
 
   def format_stat(stat_key, info_val)
-   if ['water', 'fire', 'earth', 'light', 'dark'].include?(info_val)
+    if ['water', 'fire', 'earth', 'light', 'dark'].include?(info_val)
       "<img src='/images/#{info_val}.png'/>"
     elsif  ['tank', 'attacker', 'buffer', 'healer', 'debuffer'].include?(info_val)
       "<img src='/images/#{info_val}.png'/>"
@@ -38,7 +38,7 @@ end
 def delete_selected_file(name)
   Dir.glob("./public/images/").each do |f|
 
-  FileUtils.rm(f + name)
+    FileUtils.rm(f + name)
   end
 end
 
@@ -133,6 +133,20 @@ def get_max_index_number(list)
   list = list.map { |k, v| v["index"]}.max + 1
 end
 
+def sort_by_given_info(catagory, stars, type = 'name')
+
+  star_rating = stars[0]
+  by_stars = catagory.select { |_, v| v["stars"] == star_rating }.to_h
+
+  if type == "name"
+    by_stars.sort_by { |k, v| k }.to_h
+  elsif type = "tiers"
+    by_stars.sort_by { |k, v| v["tier"] }.reverse.to_h
+  else
+    by_stars.sort_by { |k, v| v[type] }.to_h
+  end
+end
+
 get "/users/signin" do
   erb :signin
 end
@@ -146,23 +160,17 @@ post "/users/signin" do
     session[:message] = "Welcome!"
     redirect "/"
   else
-      session[:message] = "Invalid credentials!"
-      status 422
-      erb :signin
+    session[:message] = "Invalid credentials!"
+    status 422
+    erb :signin
   end
 end
 
 get "/" do
   units = load_unit_details
-  units = units.select { |_, v| v["stars"] == '5' }.to_h
-  @units = units.sort_by { |k, v| k }.to_h
-  erb :index
-end
-
-get "/show_unit_details" do
-  @unit_details = load_unit_details
-  @sc_details = load_soulcards_details
-  erb :show_unit_details
+  # units = units.select { |_, v| v["stars"] == '5' }.to_h
+  # @units = units.sort_by { |k, v| k }.to_h
+  erb :home
 end
 
 get '/download/:filename' do |filename|
@@ -174,6 +182,89 @@ get '/download/:filename' do |filename|
     send_file "./public/images/#{filename}", :filename => filename, :type => 'Application/octet-stream'
   end
   redirect "/"
+end
+
+get "/show_files/:file_name/remove" do
+  delete_selected_file(params[:file_name])
+  redirect "/show_files"
+end
+
+get "/equips/new_sc" do
+  @card = load_new_soulcard["new_sc"]
+  @max_index_val = get_max_index_number(load_soulcards_details)
+  erb :new_sc
+end
+
+get "/childs/:star_rating/:unit_name" do
+  if load_unit_details.include?(params[:unit_name]) == false
+    session[:message] = "#{params[:unit_name]} doesn't exist."
+    redirect "/"
+  end
+  @units = load_unit_details
+  @unit_name = params[:unit_name].capitalize
+  @current_unit = @units[params[:unit_name]]
+  erb :view_unit
+end
+
+get "/childs/:star_rating/:unit_name/edit" do
+  require_user_signin
+  @current_unit = load_unit_details[params[:unit_name]]
+  erb :edit_unit
+end
+
+get "/equips/:star_rating/:sc_name" do
+  @sc_name = params[:sc_name]
+  @current_card = load_soulcards_details[params[:sc_name]]
+  name = params[:sc_name]
+  erb :view_sc
+end
+
+get "/equips/:star_rating/:sc_name/edit" do
+  require_user_signin
+  @current_card = load_soulcards_details[params[:sc_name]]
+  name = params[:sc_name]
+  erb :edit_sc
+end
+
+get "/:catagory/:star_rating/:type" do
+  star_rating = params[:star_rating]
+  type = params[:type]
+  catagory = params[:catagory] == "childs" ? load_unit_details : load_soulcards_details
+
+  if star_rating.include?("5") || star_rating.include?("4") || star_rating.include?("3")
+    @units = sort_by_given_info(catagory, star_rating, type)
+  else
+    session[:message] = "There are no units by that tier!"
+    redirect "/"
+  end
+  erb :index
+end
+
+get "/:catagory/:star_rating" do
+  star_rating = params[:star_rating]
+
+  if !star_rating == "5stars" || !star_rating == "4stars" || !star_rating == "3stars"
+      session[:message] = "There are no units by that tier!"
+      redirect "/"
+  end
+
+  if params[:catagory] == "childs"
+    unit_info = load_unit_details
+    @catagory = 'childs'
+    @units = sort_by_given_info(unit_info, params[:star_rating])
+    erb :index
+  else
+    unit_info = load_soulcards_details
+    @catagory = 'equips'
+    @cards = sort_by_given_info(unit_info, params[:star_rating])
+    erb :list_sc
+  end
+end
+
+get "/show_unit_details" do
+  @unit_details = load_unit_details
+  @sc_details = load_soulcards_details
+  erb :show_unit_details
 end
 
 get "/show_files" do
@@ -188,21 +279,6 @@ get "/show_files" do
   erb :file_list
 end
 
-get "/show_files/:file_name/remove" do
-  delete_selected_file(params[:file_name])
-  redirect "/show_files"
-end
-
-get "/sort_by/:type" do
-  # @units = load_unit_details.select { |_, v| v["type"] == params[:type] }.to_h
-  if params[:type] == '4'
-    @units = load_unit_details.select { |_, v| v["stars"] == '4' }.to_h.sort_by { |k, v| k }.to_h
-  else
-    @units = load_unit_details.sort_by { |k, v| v[params[:type]] }.to_h
-  end
-  erb :index
-end
-
 get "/new_unit" do
   require_user_signin
   @new_unit_info = load_new_unit["new_unit"]
@@ -210,20 +286,14 @@ get "/new_unit" do
   erb :new_unit
 end
 
-get "/equips/new_sc" do
-  @card = load_new_soulcard["new_sc"]
-  @max_index_val = get_max_index_number(load_soulcards_details)
-  erb :new_sc
-end
-
 get "/upload" do
   erb :upload
 end
 
-get "/:unit_name" do
+get "/childs/:star_rating/:unit_name" do
   if load_unit_details.include?(params[:unit_name]) == false
-   session[:message] = "#{params[:unit_name]} doesn't exist."
-   redirect "/"
+    session[:message] = "#{params[:unit_name]} doesn't exist."
+    redirect "/"
   end
   @units = load_unit_details
   @unit_name = params[:unit_name].capitalize
@@ -231,30 +301,18 @@ get "/:unit_name" do
   erb :view_unit
 end
 
-get "/:unit_name/edit" do
+get "/childs/:unit_name/edit" do
   require_user_signin
   @current_unit = load_unit_details[params[:unit_name]]
   erb :edit_unit
 end
 
-get "/equips/soulcards" do
-  @cards = load_soulcards_details
-  erb :list_sc
-end
+# get "/equips/soulcards" do
+#   @cards = load_soulcards_details
+#   erb :list_sc
+# end
 
-get "/equips/soulcards/:sc_name" do
-  @sc_name = params[:sc_name]
-  @current_card = load_soulcards_details[params[:sc_name]]
-  name = params[:sc_name]
-  erb :view_sc
-end
 
-get "/equips/:sc_name/edit" do
-  require_user_signin
-  @current_card = load_soulcards_details[params[:sc_name]]
-  name = params[:sc_name]
-  erb :edit_sc
-end
 
 post "/equips/new_sc" do
   require_user_signin
@@ -287,32 +345,32 @@ post "/equips/new_sc" do
 
     session[:message] = "A unit by that name already exists. Please create a different card."
     if params["edited"]
-        status 422
-        temp_name = get_unit_name(unit_data, index)
-        redirect "/equips/#{temp_name}/edit"
+      status 422
+      temp_name = get_unit_name(unit_data, index)
+      redirect "/equips/#{temp_name}/edit"
     else
-        status 422
-        redirect "/equips/new_sc"
+      status 422
+      redirect "/equips/new_sc"
     end
 
   elsif params["edited"] && (unit_data.include?(name) == false && index == original_unit["index"])
     old = data.delete(original_unit.keys.first)
-      data[name] = {}
+    data[name] = {}
   else
     old = data.delete(original_unit.keys.first)
     data[name] = {}
   end
 
-    data[name]["pic"] = pname.include?(".") ? pname : (pname + ".jpg")
-    data[name]["stars"] = params[:stars]
-    data[name]["stats"] = params[:stats]
-    data[name]["passive"] = params[:passive]
-    data[name]["index"] = index
+  data[name]["pic"] = pname.include?(".") ? pname : (pname + ".jpg")
+  data[name]["stars"] = params[:stars]
+  data[name]["stats"] = params[:stats]
+  data[name]["passive"] = params[:passive]
+  data[name]["index"] = index
 
-    File.write("data/sc/soul_cards.yml", YAML.dump(data))
+  File.write("data/sc/soul_cards.yml", YAML.dump(data))
 
-    session[:message] = "New Soulcard called #{name.upcase} has been created."
-    redirect "/equips/soulcards"
+  session[:message] = "New Soulcard called #{name.upcase} has been created."
+  redirect "/equips/soulcards"
 end
 
 post "/new_unit" do
@@ -344,58 +402,58 @@ post "/new_unit" do
     #this clause makes sure we can only edit units if there are no name conflicts.
     session[:message] = "A unit by that name already exists. Please enter a different unit name."
     if params["edited"]
-        status 422
-        temp_name = get_unit_name(unit_data, index)
-        redirect "/#{temp_name}/edit"
+      status 422
+      temp_name = get_unit_name(unit_data, index)
+      redirect "/#{temp_name}/edit"
     else
-        status 422
-        redirect "/new_unit"
+      status 422
+      redirect "/new_unit"
     end
 
-    elsif params["edited"] && (unit_data.include?(name) == false && index == original_unit["index"])
-      old = data.delete(original_unit.keys.first)
-      data[name] = {}
-    else
-      old = data.delete(original_unit.keys.first)
-      data[name] = {}
+  elsif params["edited"] && (unit_data.include?(name) == false && index == original_unit["index"])
+    old = data.delete(original_unit.keys.first)
+    data[name] = {}
+  else
+    old = data.delete(original_unit.keys.first)
+    data[name] = {}
   end
 
-    data[name]["pic"] = pname.include?(".") ? pname : (pname + ".png")
+  data[name]["pic"] = pname.include?(".") ? pname : (pname + ".png")
 
-    if params[:pic2] == ''
-      data[name]["pic2"] = ''
-    elsif params[:pic2].include?("images")
-      data[name]["pic2"] = params[:pic2]
-    else
-      data[name]["pic2"] = "/images/" + params[:pic2] + ".png"
-    end
+  if params[:pic2] == ''
+    data[name]["pic2"] = ''
+  elsif params[:pic2].include?("images")
+    data[name]["pic2"] = params[:pic2]
+  else
+    data[name]["pic2"] = "/images/" + params[:pic2] + ".png"
+  end
 
-    if params[:pic3] == ''
-      data[name]["pic3"] = ''
-    elsif params[:pic3].include?("images")
-      data[name]["pic3"] = params[:pic2]
-    else
-      data[name]["pic3"] = "/images/" + params[:pic3] + ".png"
-    end
+  if params[:pic3] == ''
+    data[name]["pic3"] = ''
+  elsif params[:pic3].include?("images")
+    data[name]["pic3"] = params[:pic2]
+  else
+    data[name]["pic3"] = "/images/" + params[:pic3] + ".png"
+  end
 
-    data[name]["tier"] = params[:tier].upcase
+  data[name]["tier"] = params[:tier].upcase
 
-    data[name]["stars"] = params[:stars]
-    data[name]["type"] = params[:type]
-    data[name]["element"] = params[:element]
-    data[name]["leader"] = params[:leader]
-    data[name]["auto"] = params[:auto]
-    data[name]["tap"] = params[:tap]
-    data[name]["slide"] = params[:slide]
-    data[name]["drive"] = params[:drive]
-    data[name]["index"] = index
+  data[name]["stars"] = params[:stars]
+  data[name]["type"] = params[:type]
+  data[name]["element"] = params[:element]
+  data[name]["leader"] = params[:leader]
+  data[name]["auto"] = params[:auto]
+  data[name]["tap"] = params[:tap]
+  data[name]["slide"] = params[:slide]
+  data[name]["drive"] = params[:drive]
+  data[name]["index"] = index
 
-    File.write("data/unit_details.yml", YAML.dump(data))
-    session[:message] = "New unit called #{name.upcase} has been created."
-    redirect "/"
+  File.write("data/unit_details.yml", YAML.dump(data))
+  session[:message] = "New unit called #{name.upcase} has been created."
+  redirect "/"
 end
 
-get "/:unit_name/remove" do  #use this if using the normal links for edit/remove
+get "/childs/:star_rating/:unit_name/remove" do  #use this if using the normal links for edit/remove
   require_user_signin
   unit = params[:unit_name]
   units_info = load_unit_details
@@ -412,7 +470,7 @@ get "/:unit_name/remove" do  #use this if using the normal links for edit/remove
   end
 end
 
-get "/equips/:sc_name/remove" do  #use this if using the normal links for edit/remove
+get "/equips/:star_rating/:sc_name/remove" do  #use this if using the normal links for edit/remove
   require_user_signin
   card = params[:sc_name]
   cards_info = load_soulcards_details
